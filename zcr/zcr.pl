f2b67aa016a7ab90a0d72cc0955d3cb8339c1434
egawata <egawa.takashi@gmail.com>
2014-05-18 19:29:04 +0900

    enable to calc with stereo format / wav less than 30 secs

--- zcr/zcr.pl
+++ zcr/zcr.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl
 
 use strict;
-#use warnings;
+use warnings;
 
 use Audio::Wav;
 use File::Basename;
 use Data::Dumper;
+use List::Util qw(sum);
 
 main();
 exit(0);
@@ -19,27 +20,36 @@ sub main {
     
     my $details = $read->details();
     my $sample_rate = $details->{sample_rate};
+    my $length_sec  = $details->{length};
+    my $num_samples = $length_sec * $sample_rate;
+    my $num_channels = $details->{channels};
 
-    #  解析の対象区間は、楽曲の長さの半分の箇所から前後15秒
-    #  2 でなく 4 で割っているのは、data_length の単位が byte であって、
-    #  sample 単位に変換するためにまず 2 で割り(1sample = 16bits)、
-    #  さらに半分の箇所を求めるために 2 で割るという演算を一度に行っているため。
-    my $start = $details->{data_length} / 4 - $sample_rate * 15;
+    my $start  = int( ($length_sec / 2 - 15) * $sample_rate );
     my $length = $sample_rate * 30;
+
+    $start > 0 or $start = 0;
+    $start + $length < $num_samples
+        or $length = $num_samples - $start;
+    print "Start: $start, Length: $length\n";
 
     $read->move_to_sample($start);
 
     my $prev_amp = 0;
     my $zero_count = 0;
     for ( 1 .. $length - 1 ) {
-        my ($amp) = $read->read();
-        defined $amp or die "Failed to retrieve sample at $_\n";
+        my @channels = $read->read();
+        defined( $channels[0] )
+            or die "Failed to retrieve sample at $_\n";
+        my $amp = sum(@channels) / $num_channels;
         $prev_amp * $amp < 0 and $zero_count++;
         $prev_amp = $amp;
     }
 
     #  1ms あたりの ZCR
-    print basename($file) . "\t: ZCR = " . $zero_count / 30000 . "\n";
+    my $zcr = $zero_count / ($length / $sample_rate * 1000);
+    print basename($file) . "\t: ZCR = " . $zcr . "\n";
+
+    return $zcr;
 }
 
 
